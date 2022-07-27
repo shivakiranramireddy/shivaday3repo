dea829f3433a45fb73d779f93282760a27fd4e24
shivakiranramireddy <109954512+shivakiranramireddy@users.noreply.github.com>
2022-07-27 13:29:02 +0530

    Update HelloWorld1.java

--- HelloWorld1.java
+++ HelloWorld1.java
@@ -1,7 +1,7 @@
 /** 
  * The HelloWorldApp class implements an application that simply prints "Hello World" to standard output.
  */
-public class HelloWorld {
+public class HelloWorld1 {
   public static void main(String[] args) {
     System.out.println("Hello, World from Oracle DevOps create on July 27, 2022");
     System.out.println("tested tester1 machine as executor");
